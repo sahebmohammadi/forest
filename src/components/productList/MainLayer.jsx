dea909f162d148a917f43b954d3ca4d1a6f81236
mohammadmahdimoadabpour <m.mahdimoadabpour@gmail.com>
2020-12-14 14:36:47 +0330

    add close button in MainLayer.jsx

--- src/components/productList/MainLayer.jsx
+++ src/components/productList/MainLayer.jsx
@@ -6,9 +6,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 import ProductList from '../ProductList';
-
-
 
 
 const MainLayer = () => {
@@ -19,6 +19,8 @@ const MainLayer = () => {
         root: {
             minWidth: 275,
             maxWidth: "30%",
+            position: "relative",
+            boxShadow: "none"
         },
         bullet: {
             display: 'inline-block',
@@ -28,10 +30,16 @@ const MainLayer = () => {
         title: {
             fontSize: 14,
         },
-        pos: {
-            marginBottom: 12,
+        closeButton: {
+            position: 'absolute',
+            right: 5,
+            top: 5
         },
     });
+
+
+
+
 
     const classes = useStyles();
     const bull = <span className={classes.bullet}>•</span>;
@@ -42,10 +50,15 @@ const MainLayer = () => {
         <Card className={classes.root}>
 
             <CardContent>
+
                 <Typography variant="h5" component="h2">
                     عنوان اصلی گروه لایه‌ها
                 </Typography>
-                
+
+                <IconButton aria-label="close" className={classes.closeButton} >
+                    <CloseIcon />
+                </IconButton>
+
                 <ProductList />
             </CardContent>
         </Card>
